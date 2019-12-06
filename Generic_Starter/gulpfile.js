1d6eaa7a39310dd47009f62f509d12d84d20704e
jermbo <jermbo@cosmicstrawberry.com>
2019-12-06 13:55:49 -0500

    removing unnecessary packages to reduce package rot

--- Generic_Starter/gulpfile.js
+++ Generic_Starter/gulpfile.js
@@ -5,7 +5,7 @@ const { serve } = require("./gulp/browser-sync");
 const { default: watcher } = require("./gulp/watcher");
 const { default: html } = require("./gulp/html");
 const { default: styles } = require("./gulp/styles");
-const { default: scripts, bundle } = require("./gulp/scripts");
+const { default: scripts } = require("./gulp/scripts");
 const { cleanAll, cleanTmp } = require("./gulp/clean");
 const { default: images } = require("./gulp/images");
 
@@ -14,5 +14,3 @@ exports.clean = cleanAll;
 exports.default = series(cleanAll, parallel(html, styles, scripts, images), cleanTmp, serve, watcher);
 
 exports.compile = series(cleanAll, parallel(html, styles, scripts, images), cleanTmp);
-
-exports.js = series(cleanAll, bundle);
